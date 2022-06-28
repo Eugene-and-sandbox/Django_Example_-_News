c0b48a27ff89aedb7812a9d4c8c416b78d0a1d5f
Eugene Chernyshev <istinnijgenia@gmail.com>
2022-06-28 12:01:01 +0300

    1. Replaced static settings rows 2. Added the "static" folder again

--- settings/settings/production.py
+++ settings/settings/production.py
@@ -111,10 +111,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Django_Example_-_News/usersite/static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
