9ada48458f268408253ea2c2c603851d18e6c642
Eugene Chernyshev <istinnijgenia@gmail.com>
2022-06-28 11:49:49 +0300

    os.path.join(BASE_DIR, './usersite/static')

--- settings/settings/production.py
+++ settings/settings/production.py
@@ -114,7 +114,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '../usersite/static'),
+    os.path.join(BASE_DIR, './usersite/static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
